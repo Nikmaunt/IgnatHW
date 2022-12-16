da4cc120abc83bddb5e5989d38da5047c87c3a36
Nick <nikmaunt@gmail.com>
2022-12-16 12:57:18 +0300

    hw10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,5 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}><img className={s.img}
+                                                           src=" https://cdn3.vectorstock.com/i/1000x1000/58/87/loading-icon-load-load-icon-white-background-vector-27845887.jpg"
+                                                           alt=""/></div>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,17 +1,21 @@
+import {LoadingType} from "../HW10";
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType) => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case "CHANGE_LOADING": {
+            return {...state, isLoading: action.isLoading}
+        }
         default:
             return state
     }
 }
 
-type LoadingActionType = {
+export type LoadingActionType = {
     type: 'CHANGE_LOADING'
     isLoading: boolean
 }
